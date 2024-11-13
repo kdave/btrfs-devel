c3458282788c8d6c11002b87ed16c3b55d3f7190
Josef Bacik <josef@toxicpanda.com>
2024-11-13 11:11:55 -0500

    btrfs: fix improper generation check in snapshot delete
    We have been using the following check
    
       if (generation <= root->root_key.offset)
    
    to make decisions about whether or not to visit a node during snapshot
    delete.  This is because for normal subvolumes this is set to 0, and for
    snapshots it's set to the creation generation.  The idea being that if
    the generation of the node is less than or equal to our creation
    generation then we don't need to visit that node, because it doesn't
    belong to us, we can simply drop our reference and move on.
    
    However reloc roots don't have their generation stored in
    root->root_key.offset, instead that is the objectid of their
    corresponding fs root.  This means we can incorrectly not walk into
    nodes that need to be dropped when deleting a reloc root.
    
    There are a variety of consequences to making the wrong choice in two
    distinct areas.
    
    visit_node_for_delete()
    
    1. False positive.  We think we are newer than the block when we really
       aren't.  We don't visit the node and drop our reference to the node
       and carry on.  This would result in leaked space.
    2. False negative.  We do decide to walk down into a block that we
       should have just dropped our reference to.  However this means that
       the child node will have refs > 1, so we will switch to
       UPDATE_BACKREF, and then the subsequent walk_down_proc() will notice
       that btrfs_header_owner(node) != root->root_key.objectid and it'll
       break out of the loop, and then walk_up_proc() will drop our reference,
       so this appears to be ok.
    
    do_walk_down()
    
    1. False positive.  We are in UPDATE_BACKREF and incorrectly decide that
       we are done and don't need to update the backref for our lower nodes.
       This is another case that simply won't happen with relocation, as we
       only have to do UPDATE_BACKREF if the node below us was shared and
       didn't have FULL_BACKREF set, and since we don't own that node
       because we're a reloc root we actually won't end up in this case.
    2. False negative.  Again this is tricky because as described above, we
       simply wouldn't be here from relocation, because we don't own any of
       the nodes because we never set btrfs_header_owner() to the reloc root
       objectid, and we always use FULL_BACKREF, we never actually need to
       set FULL_BACKREF on any children.
    
    Having spent a lot of time stressing relocation/snapshot delete recently
    I've not seen this pop in practice.  But this is objectively incorrect,
    so fix this to get the correct starting generation based on the root
    we're dropping to keep me from thinking there's a problem here.
    
    Reviewed-by: Filipe Manana <fdmanana@suse.com>
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>
    [ Manually ported to 6.11 and earlier. ]
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/extent-tree.c
+++ fs/btrfs/extent-tree.c
@@ -5088,7 +5088,7 @@ static noinline void reada_walk_down(struct btrfs_trans_handle *trans,
 			goto reada;
 
 		if (wc->stage == UPDATE_BACKREF &&
-		    generation <= root->root_key.offset)
+		    generation <= btrfs_root_origin_generation(root))
 			continue;
 
 		/* We don't lock the tree block, it's OK to be racy here */
@@ -5116,7 +5116,7 @@ static noinline void reada_walk_down(struct btrfs_trans_handle *trans,
 			    (flags & BTRFS_BLOCK_FLAG_FULL_BACKREF))
 				continue;
 			if (!wc->update_ref ||
-			    generation <= root->root_key.offset)
+			    generation <= btrfs_root_origin_generation(root))
 				continue;
 			btrfs_node_key_to_cpu(eb, &key, slot);
 			ret = btrfs_comp_cpu_keys(&key,
@@ -5279,7 +5279,7 @@ static noinline int do_walk_down(struct btrfs_trans_handle *trans,
 	 * for the subtree
 	 */
 	if (wc->stage == UPDATE_BACKREF &&
-	    generation <= root->root_key.offset) {
+	    generation <= btrfs_root_origin_generation(root)) {
 		*lookup_info = 1;
 		return 1;
 	}
@@ -5325,7 +5325,7 @@ static noinline int do_walk_down(struct btrfs_trans_handle *trans,
 				goto skip;
 
 			if (!wc->update_ref ||
-			    generation <= root->root_key.offset)
+			    generation <= btrfs_root_origin_generation(root))
 				goto skip;
 
 			btrfs_node_key_to_cpu(path->nodes[level], &key,
