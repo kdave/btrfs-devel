2c8e0ac8633ac771ae43e0e5180068f49dd86d0c
Caleb Sander Mateos <csander@purestorage.com>
2025-06-19 13:27:46 -0600

    io_uring/cmd: introduce IORING_URING_CMD_REISSUE flag
    Add a flag IORING_URING_CMD_REISSUE that ->uring_cmd() implementations
    can use to tell whether this is the first or subsequent issue of the
    uring_cmd. This will allow ->uring_cmd() implementations to store
    information in the io_uring_cmd's pdu across issues.
    
    Signed-off-by: Caleb Sander Mateos <csander@purestorage.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- io_uring/uring_cmd.c
+++ io_uring/uring_cmd.c
@@ -259,6 +259,8 @@ int io_uring_cmd(struct io_kiocb *req, unsigned int issue_flags)
 	}
 
 	ret = file->f_op->uring_cmd(ioucmd, issue_flags);
+	if (ret == -EAGAIN)
+		ioucmd->flags |= IORING_URING_CMD_REISSUE;
 	if (ret == -EAGAIN || ret == -EIOCBQUEUED)
 		return ret;
 	if (ret < 0)
